0d9490a5ef04982a12664f0b9c2c9c4709e09e90
Murilo Carnelos <muriloc.c@hotmail.com>
2022-08-06 16:59:47 -0300

    Criacao do dark mode e inclusao de botao de volume nos cards

--- script.js
+++ script.js
@@ -2,16 +2,25 @@ const buttonPlay = document.querySelector('.play')
 const buttonStop = document.querySelector('.stop')
 const buttonPlus = document.querySelector('.plus')
 const buttonLess = document.querySelector('.less')
+
+const body = document.querySelector('body')
+const light = document.querySelector('.light')
+const dark = document.querySelector('.dark')
+
 const minutesDisplay = document.querySelector('.minutes')
 const secondsDisplay = document.querySelector('.seconds')
-const buttonForest = document.querySelector('.cardForest')
-const buttonRain = document.querySelector('.cardRain')
-const buttonCoffeShop = document.querySelector('.cardCoffeShop')
-const buttonFireplace = document.querySelector('.cardFireplace')
+
+const card = document.querySelector('.card')
+const buttonForest = document.querySelector('.forest')
+const buttonRain = document.querySelector('.rain')
+const buttonCoffeShop = document.querySelector('.coffeShop')
+const buttonFireplace = document.querySelector('.fireplace')
+
 const audioForest = new Audio("./sounds/Floresta.wav")
 const audioRain = new Audio("./sounds/Chuva.wav")
 const audioCoffeShop = new Audio("./sounds/Cafeteria.wav")
 const audioFireplace = new Audio("./sounds/Lareira.wav")
+
 let minutes = Number(minutesDisplay.textContent) //pega os minutos iniciais
 let timerTimeOut //utilizado para parar o timer (stop)
 
@@ -104,18 +113,87 @@ function fireplace() {
   audioFireplace.play()
 }
 
+function changeForestColor(){
+  buttonForest.classList.add('active')
+  buttonRain.classList.remove('active')
+  buttonCoffeShop.classList.remove('active')
+  buttonFireplace.classList.remove('active')
+}
+
+function changeRainColor(){
+  buttonRain.classList.add('active')
+  buttonForest.classList.remove('active')
+  buttonCoffeShop.classList.remove('active')
+  buttonFireplace.classList.remove('active')
+}
+
+function changeCoffeShopColor(){
+  buttonRain.classList.remove('active')
+  buttonForest.classList.remove('active')
+  buttonCoffeShop.classList.add('active')
+  buttonFireplace.classList.remove('active')
+}
+
+function changeFireplaceColor(){
+  buttonRain.classList.remove('active')
+  buttonForest.classList.remove('active')
+  buttonCoffeShop.classList.remove('active')
+  buttonFireplace.classList.add('active')
+}
+
 buttonForest.addEventListener('click', function() {
   forest()
+  changeForestColor()
 })
 
 buttonRain.addEventListener('click', function() {
   rain()
+  changeRainColor()
 })
 
 buttonCoffeShop.addEventListener('click', function() {
   coffeShop()
+  changeCoffeShopColor()
 })
 
 buttonFireplace.addEventListener('click', function() {
   fireplace()
+  changeFireplaceColor()
+})
+
+light.addEventListener('click', function(){
+  changeThemeToDark()
+})
+
+dark.addEventListener('click', function(){
+  changeThemeToLight()
+ 
+})
+
+function changeThemeToDark(){
+  light.classList.add('hide')
+  dark.classList.remove('hide')
+  body.classList.add('dark')
+}
+
+function changeThemeToLight(){
+  light.classList.remove('hide')
+  dark.classList.add('hide')
+  body.classList.remove('dark')
+}
+
+volForest.addEventListener('input', function () {
+  audioForest.volume = volForest.value;
+})
+
+volRain.addEventListener('input', function () {
+  audioRain.volume = volRain.value
+})
+
+volCoffe.addEventListener('input', function(){
+  audioCoffeShop.volume = volCoffe.value
+})
+
+volFireplace.addEventListener('input', function(){
+  audioFireplace.volume = volFireplace.value
 })
